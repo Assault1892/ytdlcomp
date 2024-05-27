25b766a0eebdd990b230e54e6a322d78142d0b55
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2024-05-27 10:25:08 +0900

    perhaps

--- main.py
+++ main.py
@@ -2,41 +2,16 @@ from yt_dlp import YoutubeDL
 import os
 
 filepath = os.getcwd()
-mode = 1
 
-# === URL
-url = input(str("URL: "))
-# print("入力URL: " + url)
-# print(type(url))
-
-# === mode
-
-# print("mode: " + str(mode))
-# print(type(mode))
-
-if mode == 1: # url
-    while True:
-        try:
-            dlmode = int(input("1: best mp4, 2: mp3, 3: bestaudio (wav), 4: cancel: "))
-            mode = 2
-            break
-        except ValueError:
-            pass
-    else:
-        exit
-
-if dlmode == 1: # mp4
-    dl_option = {
+def download(url, dltype):
+    if dltype == 1: # mp4
+        dl_option = {
         "restrictfilenames": "true", # ファイル名をASCII文字列に制限
         "format": "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]", # 最高品質mp4
         "outtmpl": f"{filepath}/outputs/mp4/%(title)s.%(ext)s" 
     }
-    
-    with YoutubeDL(dl_option) as ydl:
-        res = ydl.download(url)
-
-elif dlmode == 2: # mp3
-    dl_option = {
+    elif dltype == 2: # mp3
+        dl_option = {
         "restrictfilenames": "true", # ファイル名をASCII文字列に制限
         "format": "mp3/bestaudio/best",
         "outtmpl": f"{filepath}/outputs/mp3/%(title)s.%(ext)s",
@@ -47,19 +22,26 @@ elif dlmode == 2: # mp3
             }
         ]
     }
-    
-    with YoutubeDL(dl_option) as ydl:
-        res = ydl.download(url)
-
-elif dlmode == 3: # wav
-    dl_option = {
+    elif dltype == 3: # wav
+        dl_option = {
         "restrictfilenames": "true", # ファイル名をASCII文字列に制限
         "format": "wav/bestaudio/best",
         "outtmpl": f"{filepath}/outputs/wav/%(title)s.%(ext)s" 
     }
+    else:
+        print("no")
+        pass
     
     with YoutubeDL(dl_option) as ydl:
         res = ydl.download(url)
-else: # cancel
-    print("nope")
-    mode = 1
+
+url = input(str("URL: "))
+
+while True:
+    try:
+        dltype = int(input("1: best mp4, 2: mp3, 3: bestaudio (wav), 4: cancel: "))
+        break
+    except ValueError:
+        pass
+
+download(url, dltype)
